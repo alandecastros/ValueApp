abb4bdf14e8e1a6b0304a701e3f25c0e7764267d
Alan Castro <alandecastros@gmail.com>
2020-11-22 13:38:55 -0300

    removing serilog

--- ValueApp.Api/Startup.cs
+++ ValueApp.Api/Startup.cs
@@ -2,8 +2,6 @@ using MediatR;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Serilog;
 using System.Globalization;
 using System.Reflection;
 using ValueApp.Domain.Interfaces;
@@ -25,11 +23,7 @@ namespace ValueApp.Api
             var serviceProvider = builder.Services.BuildServiceProvider();
             Configuration = serviceProvider.GetService<IConfiguration>();
 
-            var logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-            builder.Services.AddLogging(lb => lb.AddSerilog(logger));
+            builder.Services.AddLogging();
 
             builder.Services.AddMediatR(Assembly.Load("ValueApp.Domain"));
 
