a412cfd9029a697eb9255e68a991968a63a24bed
Alan Castro <alandecastros@gmail.com>
2020-11-22 13:22:36 -0300

    added logging to AcoesService and added a try catch to handle possibles format exceptions

--- ValueApp.Api/Startup.cs
+++ ValueApp.Api/Startup.cs
@@ -2,6 +2,8 @@ using MediatR;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System.Globalization;
 using System.Reflection;
 using ValueApp.Domain.Interfaces;
@@ -23,12 +25,16 @@ namespace ValueApp.Api
             var serviceProvider = builder.Services.BuildServiceProvider();
             Configuration = serviceProvider.GetService<IConfiguration>();
 
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            builder.Services.AddLogging(lb => lb.AddSerilog(logger));
+
             builder.Services.AddMediatR(Assembly.Load("ValueApp.Domain"));
 
             // Services
-            builder.Services.AddScoped<IAcoesService>(s => new AcoesService(
-                googleAppName: Configuration["GOOGLE_APP_NAME"],
-                googleAppApiKey: Configuration["GOOGLE_APP_API_KEY"]));
+            builder.Services.AddScoped<IAcoesService, AcoesService>();
         }
     }
 }
--- ValueApp.Infra/Services/AcoesService.cs
+++ ValueApp.Infra/Services/AcoesService.cs
@@ -1,5 +1,7 @@
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,11 +14,13 @@ namespace ValueApp.Infra.Services
     {
         private readonly string googleAppName;
         private readonly string googleAppApiKey;
+        private readonly ILogger<AcoesService> logger;
 
-        public AcoesService(string googleAppName, string googleAppApiKey)
+        public AcoesService(IConfiguration configuration, ILogger<AcoesService> logger)
         {
-            this.googleAppName = googleAppName;
-            this.googleAppApiKey = googleAppApiKey;
+            googleAppName = configuration["GOOGLE_APP_NAME"];
+            googleAppApiKey = configuration["GOOGLE_APP_API_KEY"];
+            this.logger = logger;
         }
 
         public async Task<IList<Acao>> ObterListaAcoes()
@@ -40,14 +44,22 @@ namespace ValueApp.Infra.Services
             {
                 foreach (var row in values)
                 {
-                    var preco = Convert.ToDouble(row[1]);
-                    var lpa = Convert.ToDouble(row[2]);
-                    acoes.Add(new Acao
+                    try
                     {
-                        Simbolo = (string)row[0],
-                        Preco = preco,
-                        Lpa = lpa
-                    });
+                        var preco = Convert.ToDouble(row[1]);
+                        var lpa = Convert.ToDouble(row[2]);
+                        acoes.Add(new Acao
+                        {
+                            Simbolo = (string)row[0],
+                            Preco = preco,
+                            Lpa = lpa
+                        });
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogError($"Erro na linha: {row[0]}, {row[1]}, {row[2]}");
+                        logger.LogError(e.Message, e);
+                    }
                 }
             }
 
